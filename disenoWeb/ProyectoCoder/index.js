e55eed67635ac2c0af25c0dc6e84f95a5683e41e
Georgina <georginabosque@gmail.com>
2021-09-10 09:05:01 -0300

    manipulando el DOM

---
+++ disenoWeb/ProyectoCoder/index.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('buy-now');
+
+button.innerHTML = "Hackeado papa"
+
+button.addEventListener('click', ()=>{
+    button.innerHTML = "Gracias por tu compra";
+})
