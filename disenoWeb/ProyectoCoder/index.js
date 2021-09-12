46e22110376901c701ed74d059668ed39d191ad5
Georgina <georginabosque@gmail.com>
2021-09-12 16:49:01 -0300

    Agregar y crear elementos en el DOM

--- disenoWeb/ProyectoCoder/index.js
+++ disenoWeb/ProyectoCoder/index.js
@@ -1,7 +1,38 @@
 const button = document.getElementById('buy-now');
 
-button.innerHTML = "Hackeado papa"
-
 button.addEventListener('click', ()=>{
-    button.innerHTML = "Gracias por tu compra";
+    if(button.innerHTML == 'Comprar ahora'){
+        button.innerHTML = 'Gracias por su compra'
+    }else{
+        button.innerHTML = "Comprar ahora"
+    }
+    
+})
+
+const cards = document.querySelectorAll('.card')
+
+for(let i = 0; i<cards.length; i++){
+    const card = cards[i]
+    const indexElement = document.createElement('p')
+
+    indexElement.innerHTML = i + 1
+
+    card.appendChild(indexElement)
+
+    card.addEventListener('click', ()=>{
+        card.remove()
+    })
+}
+
+const cardsContainer = document.getElementsByClassName('catalogue-container')[0]
+const addBtn= document.getElementById('add-clothing')
+
+console.log(addBtn)
+
+addBtn.addEventListener('click', ()=>{
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    cardsContainer.appendChild(card)
+
 })
